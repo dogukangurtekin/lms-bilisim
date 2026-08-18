ee155a9cc0cfd24aed01b02fd519bd7ddbd1baab
dogukangurtekin <dogukangurtekin@gmail.com>
2026-08-19 00:55:22 +0300

    Update branding for Bilişim Kod

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     @include('partials.pwa-head')
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="description" content="Bilişim Kod - yazılım ve eğitim odaklı modern platform.">
-    <title>@yield('title', config('app.name'))</title>
+    <meta name="description" content="Bilişim Kod - yapay zeka, kodlama ve robotik odaklı modern eğitim platformu.">
+    <title>@yield('title', 'Bilişim Kod | Yapay Zeka, Kodlama, Robotik')</title>
     @php
         $pwaMeta = (array) (auth()->user()?->profile?->meta ?? []);
         $pwaConfig = [
